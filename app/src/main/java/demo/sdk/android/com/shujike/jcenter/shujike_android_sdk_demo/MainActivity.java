3e3b3d4859a9beb666c632829bb6e6ac9bf5f401
Kevin Yang <yangyongqiang@shujike.com>
2017-12-08 13:43:45 +0800

    init android sdk demo

---
+++ app/src/main/java/demo/sdk/android/com/shujike/jcenter/shujike_android_sdk_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package demo.sdk.android.com.shujike.jcenter.shujike_android_sdk_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/demo/sdk/android/com/shujike/jcenter/shujike_android_sdk_demo/MyApplication.java
@@ -0,0 +1,18 @@
+package demo.sdk.android.com.shujike.jcenter.shujike_android_sdk_demo;
+
+import android.app.Application;
+
+import com.shujike.analysis.SjkAgent;
+
+/**
+ * Created by kevin on 2017/12/8.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        SjkAgent.setDebugEnabled(true);
+        SjkAgent.init(this);
+        super.onCreate();
+    }
+}
