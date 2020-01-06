09d7c1922c7180a9b140b1ff108377fdc5990ab4
mike <chinalinux@hotmail.com>
2020-01-06 20:27:07 +0800

    bugfix

--- app/src/main/java/demo/sdk/android/com/shujike/jcenter/shujike_android_sdk_demo/MainActivity.java
+++ app/src/main/java/demo/sdk/android/com/shujike/jcenter/shujike_android_sdk_demo/MainActivity.java
@@ -2,15 +2,16 @@ package demo.sdk.android.com.shujike.jcenter.shujike_android_sdk_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.shujike.analysis.SjkAgent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-
     }
 }
 
--- app/src/main/java/demo/sdk/android/com/shujike/jcenter/shujike_android_sdk_demo/MyApplication.java
+++ app/src/main/java/demo/sdk/android/com/shujike/jcenter/shujike_android_sdk_demo/MyApplication.java
@@ -11,12 +11,13 @@ import com.shujike.analysis.SjkAgent;
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
+
+        super.onCreate();
+
         SjkAgent.setDebugEnabled(true);
 
         SjkAgent.setBaseUrl("https://t.shujike.com", "https://a.shujike.com", "ftp.shujike.com");
 
         SjkAgent.init(this);
-
-        super.onCreate();
     }
 }
